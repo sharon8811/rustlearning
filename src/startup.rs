52271d5fe2d7cf6e9a9de9466f3eb2c25aec00f7
sharon8811 <sharon8811@gmail.com>
2023-12-26 00:19:15 +0200

    add support for newsletters endpoint with sending emails

--- src/startup.rs
+++ src/startup.rs
@@ -106,6 +106,7 @@ pub fn run(
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
+            .route("/newsletters", web::post().to(publish_newsletter))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
