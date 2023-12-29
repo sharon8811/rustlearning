580255e1a426877c4d0d55b44a07d227946772d9
sharon8811 <sharon8811@gmail.com>
2023-12-30 00:25:32 +0200

    add redis as a session store middleware

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use rusttest::startup::Application;
 use rusttest::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> anyhow::Result<()> {
     let subscriber = get_subscriber(
         "rusttest".into(), "info".into(), std::io::stdout
     );
@@ -14,41 +14,3 @@ async fn main() -> Result<(), std::io::Error> {
     application.run_until_stopped().await?;
     Ok(())
 }
-
-// use actix_web::{get, web, App, HttpServer, HttpResponse, Responder, middleware::Logger};
-//
-// #[get("/hello/{name}")]
-// async fn greet(name: web::Path<String>) -> impl Responder {
-//     format!("Hello {name}!")
-// }
-//
-// async fn health_check() -> impl Responder {
-//     HttpResponse::Ok()
-// }
-//
-// #[tokio::main]
-// async fn main() -> std::io::Result<()> {
-//     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-//
-//     HttpServer::new(|| {
-//         App::new()
-//             .service(greet)
-//             .route("/health_check", web::get().to(health_check))
-//             .wrap(Logger::default())
-//     })
-//     .bind(("127.0.0.1", 8080))?
-//     .run()
-//     .await
-//     // println!("Hello, world!");
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[tokio::test]
-//     async fn health_check_test() {
-//         let response = health_check().await;
-//         assert!(response.status().is_success())
-//     }
-// }
--- src/startup.rs
+++ src/startup.rs
@@ -1,3 +1,5 @@
+use actix_session::SessionMiddleware;
+use actix_session::storage::RedisSessionStore;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use actix_web::cookie::Key;
@@ -20,7 +22,7 @@ pub struct Application {
 
 impl Application {
 
-    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+    pub async fn build(configuration: Settings) -> Result<Self, anyhow::Error> {
         let connection_pool = get_connection_pool(&configuration.database);
 
         let sender_email = configuration
@@ -47,7 +49,8 @@ impl Application {
             email_client,
             configuration.application.base_url,
             configuration.application.hmac_secret,
-        )?;
+            configuration.redis_uri,
+        ).await?;
 
         Ok(Self { port, server })
     }
@@ -62,35 +65,6 @@ impl Application {
 
 }
 
-pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
-    let connection_pool = PgPoolOptions::new()
-        .connect_lazy_with(configuration.database.with_db());
-
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout
-    );
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    run(
-        listener,
-        connection_pool,
-        email_client,
-        configuration.application.base_url,
-        configuration.application.hmac_secret
-    )
-}
-
 pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
     PgPoolOptions::new().connect_lazy_with(configuration.with_db())
 }
@@ -100,24 +74,26 @@ pub struct ApplicationBaseUrl(pub String);
 #[derive(Clone)]
 pub struct HmacSecret(pub Secret<String>);
 
-pub fn run(
+async fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
     hmac_secret: Secret<String>,
-) -> Result<Server, std::io::Error> {
+    redis_uri: Secret<String>,
+) -> Result<Server, anyhow::Error> {
     let base_url = web::Data::new(ApplicationBaseUrl(base_url));
     let db_pool = web::Data::new(db_pool);
     let email_client = web::Data::new(email_client);
-    let message_store = CookieMessageStore::builder(
-        Key::from(hmac_secret.expose_secret().as_bytes())
-    ).build();
+    let secret_key = Key::from(hmac_secret.expose_secret().as_bytes());
+    let message_store = CookieMessageStore::builder(secret_key.clone()).build();
     let message_framework = FlashMessagesFramework::builder(message_store).build();
+    let redis_store = RedisSessionStore::new(redis_uri.expose_secret()).await?;
     let server = HttpServer::new(move || {
         App::new()
             // Middlewares are added using the `wrap` method on `App`
             .wrap(message_framework.clone())
+            .wrap(SessionMiddleware::new(redis_store.clone(), secret_key.clone()))
             .wrap(TracingLogger::default())
             .route("/", web::get().to(home))
             .route("/login", web::get().to(login_form))
